475368c9d6cd22765e25d7c19f6c7aa6c7779dde
bryansantosgodi16-lang <bryansantosgodi16@gmail.com>
2026-09-09 21:41:13 -0300

    primeiro commit

---
+++ src/main/java/com/projeto/branco/BrancoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.branco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/branco/BrancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.branco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
